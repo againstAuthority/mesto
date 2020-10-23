f1a5e70c818a5fdf9e55dd0aaa1c132c9edd99eb
againstAuthority <49537476+againstAuthority@users.noreply.github.com>
2020-10-23 14:49:20 +0300

    Переименовать классы кнопок

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
-let profile = document.querySelector('.profile');
-let profileEditBtn = document.querySelector('.profile__button-edit');
 let popup = document.querySelector('.popup');
 let popupCloseBtn = popup.querySelector('.popup__close-button');
 let popupSaveBtn = popup.querySelector('.popup__save-button');
+let popupProfileName = popup.querySelector('.popup__item-profile-name');
+let popupProfileInfo = popup.querySelector('.popup__item-profile-about');
+
+let profileEditBtn = document.querySelector('.profile__edit-button');
+let profileName = document.querySelector('.profile__name');
+let profileInfo = document.querySelector('.profile__info');
+
 
 function showEditPopup() {
     popup.classList.add('popup_opened');
     getProfileData();
-}
-
-function getProfileData() {
-    let profileName = profile.querySelector('.profile__name');
-    let profileInfo = profile.querySelector('.profile__about');
-    let popupProfileName = popup.querySelector('.popup__item-profile-name');
-    let popupProfileInfo = popup.querySelector('.popup__item-profile-about');
-
-    popupProfileName.value = profileName.textContent;
-    popupProfileInfo.value = profileInfo.textContent;
-}
-
-function saveProfile() {
-    let profileName = document.querySelector('.profile__name');
-    let profileInfo = document.querySelector('.profile__about');
-    let popupProfileName = popup.querySelector('.popup__item-profile-name');
-    let popupProfileInfo = popup.querySelector('.popup__item-profile-about');
-
-    profileName.textContent = popupProfileName.value;
-    profileInfo.textContent = popupProfileInfo.value;
-
-    closeEditPopup();
 }
 
 function closeEditPopup() {
@@ -36,7 +19,18 @@ function closeEditPopup() {
     popup.classList.remove('popup_opened');
 }
 
+function getProfileData() {
+    popupProfileName.value = profileName.textContent;
+    popupProfileInfo.value = profileInfo.textContent;
+}
+
+function setProfileData() {
+    profileName.textContent = popupProfileName.value;
+    profileInfo.textContent = popupProfileInfo.value;
+
+    closeEditPopup();
+}
 
 profileEditBtn.addEventListener('click', showEditPopup);
 popupCloseBtn.addEventListener('click', closeEditPopup);
-popupSaveBtn.addEventListener('click', saveProfile);
+popupSaveBtn.addEventListener('click', setProfileData);
